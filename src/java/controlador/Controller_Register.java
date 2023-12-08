b12fc319029a4a570babc029a5a8d397cb62f2c4
Miguel13fdf <miguel.haro.est@tecazuay.edu.ec>
2023-12-08 00:38:07 -0500

    exitoregistrp

--- src/java/controlador/Controller_Register.java
+++ src/java/controlador/Controller_Register.java
@@ -1,9 +1,12 @@
 package controlador;
 
+import java.awt.event.KeyEvent;
+import java.util.List;
 import javax.swing.JOptionPane;
 import vistas.*;
 import ws.Peticiones;
 import ws.Peticiones_Service;
+import ws.Rol;
 
 /**
  *
@@ -14,9 +17,10 @@ public class Controller_Register {
     Peticiones_Service servicio = new Peticiones_Service();
     Peticiones app = servicio.getPeticionesPort();
     private Register_View register_View;
-    int idRol = 1;
-    int idUsuario = 1; // Debes implementar tu propia lógica para obtener el próximo ID
-    int idPersona = 1;
+    int idRol = app.listarroles().size();
+    int idUsuario = app.listarUsuarios().size();
+    ; // Debes implementar tu propia lógica para obtener el próximo ID
+    int idPersona = app.listarPersonas().size();
 
     public Controller_Register(Register_View register_View) {
         this.register_View = register_View;
@@ -33,17 +37,68 @@ public class Controller_Register {
             }
         });
         register_View.getBtnRegistrar().addActionListener(l -> RegistrarseBtn());
+        register_View.getTxtnombre().addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                char caracter = evt.getKeyChar();
+
+                if (!Character.isLetter(caracter) && caracter != KeyEvent.VK_BACK_SPACE) {
+                    evt.consume();
+                }
+            }
+        });
+        register_View.getTxtapellido().addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                char caracter = evt.getKeyChar();
+
+                if (!Character.isLetter(caracter) && caracter != KeyEvent.VK_BACK_SPACE) {
+                    evt.consume();
+                }
+            }
+        });
+        register_View.getTxtCedula().addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                char caracter = evt.getKeyChar();
+                // Solo permitir números y el backspace
+                if (!Character.isDigit(caracter) && caracter != KeyEvent.VK_BACK_SPACE) {
+                    evt.consume();
+                }
+            }
+        });
+        register_View.getTxtDirreccion().addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                char caracter = evt.getKeyChar();
+                // Solo permitir números y el backspace
+                if (!Character.isDigit(caracter) && caracter != KeyEvent.VK_BACK_SPACE) {
+                    evt.consume();
+                }
+            }
+        });
+
+        llenarComboBoxRoles();
+        register_View.getBtnRegistrar().addActionListener(l -> RegistrarseBtn());
+    }
+
+    private void llenarComboBoxRoles() {
+
+        List<Rol> roles = app.listarroles();
+
+        register_View.getjComboBox1().removeAllItems();
+
+        for (Rol rol : roles) {
+            register_View.getjComboBox1().addItem(rol.getRol());
+        }
     }
 
     public void RegistrarseBtn() {
-        
-        try {
-            
-        
+
         // Obtener los datos del usuario
         int idUsuario = obtenerProximoIdUsuario(); // Debes implementar tu propia lógica para obtener el próximo ID
         int idPersona = obtenerProximoIdPersona(); // Debes implementar tu propia lógica para obtener el próximo ID
-        String nombreUsuario = register_View.getTxtnombre().getText();
+        String nombreUsuario = register_View.getTxtuser().getText();
         String contraseña = register_View.getTxtpassword().getText();
         String nombrePersona = register_View.getTxtnombre().getText();
         String apellidoPersona = register_View.getTxtapellido().getText();
@@ -51,22 +106,59 @@ public class Controller_Register {
         String celularPersona = register_View.getTxtNTelefono().getText();
         String correoPersona = register_View.getTxtemail().getText();
 
-        // Otros datos de la persona...
-        idRol = obtenerProximoIdRol(); // Debes implementar tu propia lógica para obtener el próximo ID
+        // Validar nombre y apellido (solo letras)
+//        if (!esCadenaValida(nombrePersona) || !esCadenaValida(apellidoPersona)) {
+//            JOptionPane.showMessageDialog(null, "Nombre y apellido deben contener solo letras.");
+//            return;
+//        }
+//
+//        // Validar cédula (solo números)
+//        if (!esNumeroValido(dniPersona)) {
+//            JOptionPane.showMessageDialog(null, "La cédula debe contener solo números.");
+//            return;
+//        }
+//
+//        // Validar teléfono (10 dígitos)
+//        if (!esTelefonoValido(celularPersona)) {
+//            JOptionPane.showMessageDialog(null, "El teléfono debe contener 10 dígitos.");
+//            return;
+//        }
+//
+//        // Validar correo electrónico
+//        if (!esCorreoValido(correoPersona)) {
+//            JOptionPane.showMessageDialog(null, "Correo electrónico no válido.");
+//            return;
+//        }
+        int idRol = obtenerIdRolSeleccionado();
         String nombreRol = register_View.getjComboBox1().getSelectedItem().toString();
         boolean estadoRol = true;
-        boolean registro = app.registrarUsuarioPersonaRol(1, 1, nombreUsuario, contraseña, nombrePersona, apellidoPersona, dniPersona, celularPersona, correoPersona, 1, nombreRol, true);
-        if (registro) {
-            JOptionPane.showMessageDialog(register_View, "Registro correcto");
-        } else {
-            JOptionPane.showMessageDialog(register_View, "Registro incorrecto");
+        System.out.println(idUsuario + " idUsuario");
+        System.out.println(idPersona + " idPersona");
+        boolean registroExitoso = app.registrarUsuarioPersonaRol(idUsuario, idPersona, nombreUsuario, contraseña, nombrePersona, apellidoPersona, dniPersona, celularPersona, correoPersona, idRol, nombreRol, estadoRol);
 
+        if (registroExitoso) {
+            JOptionPane.showMessageDialog(null, "Registro exitoso.");
+            register_View.dispose();
+            Login_View login_View = new Login_View();
+            Controller_Login login = new Controller_Login(login_View);
+            login.IniciarControl();
+        } else {
+            JOptionPane.showMessageDialog(null, "Error en el registro. Por favor, revise sus datos.");
+            // Limpiar campos de texto
+            //limpiarCamposTexto();
         }
-        
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(register_View, e);
+
+    }
+
+    private int obtenerIdRolSeleccionado() {
+        // Obtener el ID del rol seleccionado en el JComboBox
+        String nombreRol = register_View.getjComboBox1().getSelectedItem().toString();
+        for (Rol rol : app.listarroles()) {
+            if (rol.getRol().equals(nombreRol)) {
+                return rol.getIdRol();
+            }
         }
-        
+        return -1; // Si no se encuentra el rol, devolver -1
     }
 
     public int obtenerProximoIdUsuario() {
@@ -91,5 +183,28 @@ public class Controller_Register {
         Login_View vl = new Login_View();
         Controller_Login ctl = new Controller_Login(vl);
         ctl.IniciarControl();
+    }
+      public static boolean esCorreoValido(String correo) {
+        // Utiliza una expresión regular para validar el formato del correo electrónico
+        String regexCorreo = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+        return correo.matches(regexCorreo);
+    }
+
+    public static boolean esTelefonoValido(String telefono) {
+        // Utiliza una expresión regular para validar que el teléfono tenga 10 dígitos
+        String regexTelefono = "\\d{10}";
+        return telefono.matches(regexTelefono);
+    }
+
+    public static boolean esNumeroValido(String numero) {
+        // Utiliza una expresión regular para validar que la cadena contenga solo números
+        String regexNumero = "\\d+";
+        return numero.matches(regexNumero);
+    }
+
+    public static boolean esCadenaValida(String cadena) {
+        // Utiliza una expresión regular para validar que la cadena contenga solo letras
+        String regexLetras = "^[a-zA-Z]+$";
+        return cadena.matches(regexLetras);
     }
 }
